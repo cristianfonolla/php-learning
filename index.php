aff62d8cf9c559784d35986190cea471e2139e9f
cristian <you@example.com>
2016-09-15 21:33:11 +0200

    codi html que no sempre es igual

--- index.php
+++ index.php
@@ -5,11 +5,20 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title> PHP-DAM </title>
 </head>
 
     <body>
-        <h1><?php echo "Hola mon!";?></h1>
+
+        <h1>
+
+            <?php
+
+                echo "Hola" . $_GET{'name'} . "!";
+
+            ?>
+        </h1>
+
     </body>
 
 </html>
