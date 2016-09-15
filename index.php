7b0e0092656cbbdf5084e695259fbca7322eed4d
cristian <you@example.com>
2016-09-15 21:58:17 +0200

    nou index php en require

--- index.php
+++ index.php
@@ -1,27 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> PHP-DAM </title>
-</head>
+<?php
 
-    <body>
+$greeting = "Hola $_GET{'name'}!";
 
-        <h1>
-
-            <?php
-//                $name = $_GET{'name'};
-//                echo "Hola " . $name . "!";
-
-
-
-            ?>
-            <?= 'Hola ' . htmlspecialchars($_GET{'name'}) . '!'  ?>
-        </h1>
-
-    </body>
-
-</html>
+require 'index.template.php';
