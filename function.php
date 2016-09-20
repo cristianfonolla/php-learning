bda34563041276283847d668001aabee4b988b41
cristian <you@example.com>
2016-09-20 21:16:49 +0200

    index.php 95% optimitzat

--- function.php
+++ function.php
@@ -9,3 +9,30 @@ function hellovtortosa($name, $sn1 = 'Curto'){
     echo "Hola " . $name . " " . "$sn1" . '!';
 
 }
+
+function connect() {
+
+    try {
+
+        $pdo = new PDO('mysql:host=127.0.0.1;dbname=prova', 'root', '');
+        return $pdo;
+
+
+    } catch (PDOException $e) {
+
+        die("Ha hagut un error durant la conexio." . $e->getMessage());
+
+
+    }
+    return $pdo;
+}
+
+function allTasks($pdo)
+{
+    $query = $pdo->prepare("SELECT * FROM todos");
+
+    $query->execute();
+
+    $tasks = $query->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, Task::class);
+    return $tasks;
+}
--- index.php
+++ index.php
@@ -5,29 +5,13 @@ require 'function.php';
 require 'Task.php';
 
 
-// POO: Php Data Objects library/biblioteca
+// PDO: Php Data Objects library/biblioteca
+
+$pdo = connect();
+
+$tasks = allTasks($pdo);
 
 
-try {
-
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=prova' , 'root' , '');
-
-
-}
-catch(PDOException $e ) {
-
-    die("Ha hagut un error durant la conexio." . $e->getMessage());
-
-
-}
-
-$query = $pdo->prepare("SELECT * FROM todos");
-
-$query->execute();
-
-$tasks = $query->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE,Task::class);
-
-$task1 = $tasks[0];
 
 
 require 'index.template.php';
