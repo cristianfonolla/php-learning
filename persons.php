798a462bf873b2697f74cb5f07ba47277a945741
cristian <you@example.com>
2016-09-22 20:42:36 +0200

    Creacio fitxer persons i modificacio persons.template.php

---
+++ persons.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'function.php';
+require 'Task.php';
+require 'database/Connection.php';
+require 'database/QueryBuilder.php';
+$config = require 'config/database.php';
+$message = require 'config/message.php';
+
+$pdo = Connection::make($config,$message);
+$query = new QueryBuilder($pdo);
+$tasks = $query->all($pdo, 'persons');
+require 'persons.template.php';
+
+
+//DRY: Dont Repeat Yourself
+//WET: Write Everithing Twice
